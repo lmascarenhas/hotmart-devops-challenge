f49a2de987af0dc542bcb7e45afe3c39d1c74132
Luiz Mascarenhas <luiz@mascarenhas.it>
2022-03-14 21:51:28 -0300

    Created S3 bucket

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "hotmart-challenge-lmascarenhas"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    id = "files"
+
+    status = "Enabled"
+
+    transition {
+      days          = 90
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 365
+      storage_class = "GLACIER"
+    }
+  }
+}
